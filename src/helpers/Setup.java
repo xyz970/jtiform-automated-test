d8830cbc9e47fc47d5de490a60b9e3957faca320
mhmmdadisptro <muhammadxxz7@gmail.com>
2025-04-20 01:20:17 +0700

    Initial Push
    Added login test with 10 different scenario

---
+++ src/helpers/Setup.java
@@ -0,0 +1,46 @@
+package helpers;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Setup {
+	
+	private static WebDriver driver;
+	private static DriverChoice driverChoice;
+	Setup setup;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+    
+    public static DriverChoice getActiveDriver() {
+    	return driverChoice;
+    }
+   
+
+    @BeforeSuite
+    public void beforeSuite() {
+    	System.out.print("Testing Dimulai");
+    	DriverChoice dChoice = DriverChoice.CHROME; 
+		DriverHelper driverHelper = new DriverHelper();
+		driver = driverHelper.setSelectedDriver(dChoice);
+		driverChoice = dChoice;
+    }
+
+    @AfterSuite
+    public void afterSuite() throws InterruptedException {
+    	System.out.print("Testing Selesai");
+        driver.quit();
+    }
+	
+	
+	public static String getParentDirectory() {
+		String parentDir = "C:\\mydata\\testing\\testng-jtiform\\";
+		return parentDir;
+	}
+	
+	public static String getWebUrl() {
+		return "https://project-jti.polije.ac.id/jtiform";
+	}
+}
