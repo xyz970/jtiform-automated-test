c3bf759562fbf103fbfe381d7040fcd4842596e5
mhmmdadisptro <muhammadxxz7@gmail.com>
2025-05-17 23:54:31 +0700

    Update ForgotPasswordTest.java
    Add 3 different email test

--- src/performance/ForgotPasswordTest.java
+++ src/performance/ForgotPasswordTest.java
@@ -11,12 +11,18 @@ import helpers.Setup;
 
 public class ForgotPasswordTest {
   
+//  @Test(priority = 5,groups = "afterForgotPassword")
   @Test(dependsOnGroups = "afterLogout",priority = 5,groups = "afterForgotPassword")
-  public void forgotPassword() {
+  public void forgotPassword() throws InterruptedException {
 	  WebDriver driver = Setup.getDriver();
+	  String emailArray[] = {"","gmail@gmail.com","e41212101@student.polije.ac.id"};
 	  driver.findElement(By.partialLinkText("Forgot your password?")).click();
-	  driver.findElement(By.name("email")).sendKeys("e41212101@student.polije.ac.id");
-	  driver.findElement(By.cssSelector("button[type='submit']")).click();
+	  for (String email : emailArray) {
+		  driver.findElement(By.name("email")).clear();
+		  driver.findElement(By.name("email")).sendKeys(email);
+		  driver.findElement(By.cssSelector("button[type='submit']")).click();
+		  Thread.sleep(2000);
+	}
 //	  assertTrue(driver.findElements(By.cssSelector("div[class='font-medium text-sm text-green-600 mb-4']")).size() > 0);
 	  driver.get(Setup.getWebUrl()+"/login");
 	  
